b466390b96f6c93f96da2016b0c164080d626541
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 19:22:46 -0400

    nested pages

---
+++ src/pages/company/history.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+// para acceder a este componente en la url se debe escribir localhost:8000/company/history
+//la carpeta company por sí sola en la url no entrega nada debes crear index en esa carpeta para que tenga su home
+const history = () => {
+  return <div>this is history of our company</div>
+}
+
+export default history
