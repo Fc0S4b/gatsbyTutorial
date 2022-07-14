7883ae14d1a3432711680c4475f836fa4595beef
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 20:29:01 -0400

    Links

--- src/pages/company/history.js
+++ src/pages/company/history.js
@@ -1,9 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 // para acceder a este componente en la url se debe escribir localhost:8000/company/history
 //la carpeta company por sí sola en la url no entrega nada debes crear index en esa carpeta para que tenga su home
 const history = () => {
-  return <div>this is history of our company</div>
+  return (
+    <div>
+      <h2>this is history of our company</h2>
+      <Link to="/">home page</Link>
+    </div>
+  )
 }
 
 export default history
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!!!</div>
+  return (
+    <div>
+      <h1> Hello world!!!</h1>
+      <div>
+        {/* no refresh page with Link */}
+        <Link to="/about">about</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+      {/* regular refresh with a link */}
+      <a href="/about">regular link</a>
+    </div>
+  )
 }
