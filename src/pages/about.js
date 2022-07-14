568b141df2912b64b600f129ee0bc7b7003b9fde
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-14 16:09:20 -0400

    css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
+import * as aboutStyles from "../examples/about.module.css"
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={aboutStyles.page}>
         <h1> about page</h1>
+        <h1> hello world</h1>
+        <p className={aboutStyles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, harum
+          corrupti. Mollitia exercitationem eius eligendi ad ullam vero eum
+          error?
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <p className={text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          optio eos, distinctio ipsam nihil reiciendis? Saepe odio deleniti
+          velit perspiciatis!
+        </p>
       </div>
     </Layout>
   )
