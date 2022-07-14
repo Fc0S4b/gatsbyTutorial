c78146a39f11fc80d2837da8ee6ee02babb46a5f
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-14 16:21:26 -0400

    styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,35 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as aboutStyles from "../examples/about.module.css"
+import styled from "styled-components"
 const about = () => {
   return (
     <Layout>
-      <div className={aboutStyles.page}>
-        <h1> about page</h1>
-        <h1> hello world</h1>
-        <p className={aboutStyles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, harum
-          corrupti. Mollitia exercitationem eius eligendi ad ullam vero eum
-          error?
-        </p>
-      </div>
+      <Wrapper>
+        <div>
+          <h1> about page</h1>
+          <h1> hello world</h1>
+          <p className="text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi,
+            harum corrupti. Mollitia exercitationem eius eligendi ad ullam vero
+            eum error?
+          </p>
+        </div>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: red;
+
+  h1 {
+    color: yellowgreen;
+  }
+
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
