f413ec92b3f966f2db23efc36f065972ff5a690c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-14 15:51:53 -0400

    Css naming issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,10 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
-      {/* inline style more specific, applies first */}
-      <h1 style={{ color: "red", textTransform: "center" }}> home page</h1>
-      <div>
-        <Link to="/about">about</Link>
-      </div>
-      <div>
-        <Link to="/company/history">history</Link>
+      <div className="home-heading">
+        <h1>home page</h1>
+        <h1>hello world</h1>
+        <h1>hello people</h1>
       </div>
     </Layout>
   )
