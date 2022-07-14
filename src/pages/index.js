f94ce016688603f7e3262637c6e89ccc170b14e1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 20:39:05 -0400

    Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Navbar from "../components/Navbar"
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1> Hello world!!!</h1>
       <div>
         {/* no refresh page with Link */}
